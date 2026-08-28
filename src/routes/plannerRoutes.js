10235cd8498f29889e9dfa38da9890157ddf1baf
hnr83 <150466798+hnr83@users.noreply.github.com>
2026-08-28 19:40:08 -0300

    Expose planner comparison route

--- src/routes/plannerRoutes.js
+++ src/routes/plannerRoutes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const {
   listScenarios,
   getScenario,
+  getScenarioComparison,
   createScenario,
   deleteScenario,
 } = require("../controllers/plannerController");
 
 router.get("/scenarios", listScenarios);
+router.get("/scenarios/:id/comparison", getScenarioComparison);
 router.get("/scenarios/:id", getScenario);
 router.post("/scenarios", createScenario);
 router.delete("/scenarios/:id", deleteScenario);
