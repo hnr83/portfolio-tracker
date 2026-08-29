707e941adef3f991783b04347f98e4f737e730bf
hnr83 <150466798+hnr83@users.noreply.github.com>
2026-08-29 16:47:37 -0300

    Route portfolio summary through realized PnL controller

--- src/routes/portfolioRoutes.js
+++ src/routes/portfolioRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { getSummary } = require('../controllers/summaryController');
 const {
-  getSummary,
   getPositions,
   getInvestments,
   getHoldings,
