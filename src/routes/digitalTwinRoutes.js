c7e61bd739508859ed4cb6ad54fc0f19a659aedd
hnr83 <150466798+hnr83@users.noreply.github.com>
2026-09-07 15:33:42 -0300

    feat: expose Twin interview session state

--- src/routes/digitalTwinRoutes.js
+++ src/routes/digitalTwinRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { getInvestorProfile, saveInvestorProfile, guidedInvestorInterview } = require("../controllers/digitalTwinController");
+const { getInvestorProfile, saveInvestorProfile, getInvestorInterviewSession, guidedInvestorInterview } = require("../controllers/digitalTwinController");
 
 const router = express.Router();
 
 router.get("/investor-profile", getInvestorProfile);
 router.put("/investor-profile", saveInvestorProfile);
+router.get("/investor-profile/interview", getInvestorInterviewSession);
 router.post("/investor-profile/interview", guidedInvestorInterview);
 
 module.exports = router;
